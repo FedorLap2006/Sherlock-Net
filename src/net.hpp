6e7d710a85865efe37dc00ef9eb21d46b5589f3e
FedorLap2006 <fe.lap.prog@gmail.com>
2019-02-17 21:44:24 +0300

    base network arch

---
+++ src/net.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iterator>
+
+class Net
+{
+public:
+	Net();
+	~Net();
+	
+};
---
+++ src/neuron.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iterator>
+
+class Neuron
+{
+public:
+	Neuron();
+	~Neuron();
+	
+};
